bef5016d0a8ace848b0078521593719b0a9dfad3
Cong-Xiong <andy.xiongcong@gmail.com>
2023-02-01 13:49:30 +0000

    fix bugs

--- examples/lock/magnet.py
+++ examples/lock/magnet.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python3
 from motors import Motors 
 from time import time, sleep 
 
 magnet = Motors() 
 port = 0         
 speed = 100          
-#run_time = 2         
 
  
-#magnet.move_motor(port,speed)      
-#start_time = time() 
-
-#while time() < start_time + run_time:
-#	pass
- 
-#magnet.stop_motors() 
-def lock(){
+def lock():
 	magnet.move_motor(port,speed)      
-}
 
-def unlock(){
+
+def unlock():
 	magnet.stop_motors() 
-}
+
