77da6dc865e4983f90e09f28a0c04a4868ea1020
SDP8 <w.boncela@gmail.com>
2023-02-02 23:24:17 +0000

    ros packages, docker changed

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,2 @@
-docker run --privileged --rm -it --mount type=bind,source="/home/sdp8/inbox/Box-hardware",target="/inbox/Box-hardware" inbox_ros_galactic
+docker run --privileged --rm -it --workdir /inbox/Box-hardware --mount type=bind,source="/home/sdp8/inbox/Box-hardware",target="/inbox/Box-hardware" \
+ inbox_ros_galactic
