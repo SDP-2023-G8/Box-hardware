3253e3c1a9936062d0e848871818c4e8eab9c15b
wojciech <w.boncela@gmail.com>
2023-02-08 13:00:24 +0000

    Launch file and params created

--- src/qr_scanner_pipeline/qr_scanner_pipeline/qr_scanner.py
+++ src/qr_scanner_pipeline/qr_scanner_pipeline/qr_scanner.py
@@ -9,8 +9,9 @@ import cv2
 
 class QRCodeNode(Node):
     def __init__(self):
-        super().__init__("QRCodeNode")
+        super().__init__("qr_code_node")
         self.get_logger().info("*** Initializing QR scanner node. ***")
+        self.init_params()
 
         # Camera
         self.cap_ = cv2.VideoCapture(0)
@@ -19,10 +20,21 @@ class QRCodeNode(Node):
 
         # QR message publisher
         self.qr_dec_pub_ = self.create_publisher(String, "~/qr_decoded", 10)
-        timer_period = 0.02 # Seconds
+        timer_period = 1. / self.cam_fps_.value # Seconds
         self.pub_timer = self.create_timer(timer_period, self.publish_qr_decoded_message)
         self.get_logger().info("Publishing decoded QR to %s" % self.qr_dec_pub_.topic_name)
         self.get_logger().info("*** QR scanner node initialized successfully. ***")
+
+    def init_params(self):
+        self.get_logger().info("*** Initializing parameters. ***")
+
+        self.display_ = self.declare_parameter("display", False)
+        self.get_logger().info("display: {0}".format(self.display_.value))
+
+        self.cam_fps_ = self.declare_parameter("cam_fps", 15)
+        self.get_logger().info("cam_fps: {0}".format(self.cam_fps_.value))
+
+        self.get_logger().info("*** Parameters initialized sucessfully ***")
     
     def publish_qr_decoded_message(self):
         self.get_logger().debug("QR message publisher called.")
@@ -40,9 +52,11 @@ class QRCodeNode(Node):
         except Exception as e:
             self.get_logger().warn("Exception while detecting QR: %s." % e.__str__())
             return
+        
+        if self.display_.value:
+            cv2.imshow("QR", img)
+            cv2.waitKey(1)
 
-        cv2.imshow("QR", img)
-        cv2.waitKey(1)
         # Publish 
         if data:
             self.get_logger().debug("QR code detected and decoded %s." % data)
