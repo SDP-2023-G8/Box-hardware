e6dff37deab7fa8b6272401958fa8614496b65eb
RaspberryPI <sdp8@raspberrypi.local>
2023-02-10 13:08:37 +0000

    SM working

--- src/state_machine/state_machine/state_machine.py
+++ src/state_machine/state_machine/state_machine.py
@@ -1,11 +1,12 @@
 # Python
 from enum import Enum
-import asyncio
+import time
 
 # ROS
 import rclpy
 from rclpy.node import Node
-from rclpy.executors import SingleThreadedExecutor
+from rclpy.qos import QoSProfile, QoSHistoryPolicy
+from rclpy.executors import SingleThreadedExecutor, MultiThreadedExecutor
 from std_srvs.srv import SetBool
 from std_msgs.msg import String
 
@@ -33,7 +34,9 @@ class StateMachine(Node):
         self.init_params()
 
         # Current state publisher
-        self.state_pub_ = self.create_publisher(self.STATE_PUB_TYPE, self.STATE_PUB_TOPIC_NAME_, 10)
+        # qos = QoSProfile()
+        # qos.history
+        self.state_pub_ = self.create_publisher(self.STATE_PUB_TYPE, self.STATE_PUB_TOPIC_NAME_, 1)
         self.get_logger().info("Current state publisher topic name: {0}".format(self.state_pub_.topic_name))
         self.pub_timer = self.create_timer(1. / self.state_pub_freq_.value, self.publish_state)
 
@@ -54,72 +57,59 @@ class StateMachine(Node):
         self.get_logger().info("*** Parameters initialized successfully ***")
 
     def set_state_(self, new_state, **kwargs):
-        self.get_logger().info("State {0} requested.".format(str(self.CURRENT_STATE_)))
+        self.get_logger().info("State {0} requested.".format(str(new_state)))
+        if self.CURRENT_STATE_ == new_state:
+            return
 
         if new_state == State.WAITING_FOR_QR:
-            # Lock the door
-            result = self.send_door_request(True)
-            self.get_logger().debug("Door lock response: {0}.".format(result))
-            if not result.success:
-                self.get_logger().error("Failed to lock the door.")
-            self.get_logger().info("Door locked.")
-
-            # Start QR subscription
-            self.qr_msg_subscription= self.create_subscription(
-                self.QR_MSG_TYPE, 
-                self.QR_TOPIC_NAME, 
-                self.qr_msg_callback,
-                10)
+            # Lock the door and start listening
+            # Asynchronous programming prevents deadlocks
+            self.door_req_future = self.send_door_request(True)
+            self.init_waiting_state()
             self.get_logger().info("Created a new subscription to {0}".format(self.qr_msg_subscription.topic_name))
-            self.CURRENT_STATE_ = State.WAITING_FOR_QR
 
         elif new_state == State.DOOR_OPENED:
             qr_msg = kwargs['qr_msg']
             self.get_logger().info("Veryfing QR: {0}".format(qr_msg))
             if self.verifyQR(qr_msg):
                 self.get_logger().info("QR code authorized.")
-                result = self.send_door_request(False)
-                self.get_logger().debug("Door lock response: {0}.".format(result))
-                if not result.success:
-                    self.get_logger().error("Failed to unlock the door.")
-                self.get_logger().info("Door unlocked.")
-                # Stop listening for QR messages
-                self.qr_msg_subscription.destroy()
-                self.CURRENT_STATE_ = State.DOOR_OPENED
-                self.lock_future = self.hold_door_open(5)
+                self.door_req_future = self.send_door_request(False)
+                self.init_open_state()
+                self.get_logger().info("Door opened for 5s")
+                time.sleep(5)
+                self.door_req_future = self.send_door_request(True)
+                self.set_state_(State.WAITING_FOR_QR)
             else:
                 self.get_logger().info("QR code not authorized.")
 
-    async def hold_door_open(self, delay):
-        self.get_logger().info("Door opened for {0}s.".format(delay))
-        await asyncio.sleep(delay)
-        self.get_logger().info("Locking the door...")
-
-        # Lock the door
-        result = self.send_door_request(True)
-        self.get_logger().debug("Door lock response: {0}.".format(result))
-        if not result.success:
-            self.get_logger().error("Failed to unlock the door.")
-            return False
+    def init_waiting_state(self):
         self.get_logger().info("Door locked.")
-
-        # Change the state
-        self.set_state_(State.WAITING_FOR_QR)
+        self.CURRENT_STATE_ = State.WAITING_FOR_QR
+        # Start QR subscription
+        self.qr_msg_subscription= self.create_subscription(
+            self.QR_MSG_TYPE, 
+            self.QR_TOPIC_NAME, 
+            self.qr_msg_callback,
+            10)
         return True
+
+    def init_open_state(self):
+        self.CURRENT_STATE_ = State.DOOR_OPENED
 
     def send_door_request(self, data):
         door_req = self.LOCK_SERVICE_TYPE.Request()
         door_req.data = data
-        door_future = self.door_lock_client_.call_async(door_req)
+        door_fut = self.door_lock_client_.call_async(door_req)
         self.get_logger().info("Sending a request to the door lock service")
-        rclpy.spin_until_future_complete(self, door_future)
-        return door_future.result()
+        return door_fut
       
     def verifyQR(self, msg):
         # TODO
         return True
     
     def qr_msg_callback(self, msg):
+        if self.CURRENT_STATE_ != State.WAITING_FOR_QR:
+            return
         msg_content = msg.data
         self.get_logger().info("Received a QR message: {0}".format(msg_content))
         self.set_state_(State.DOOR_OPENED, qr_msg=msg_content)
@@ -135,14 +125,14 @@ class StateMachine(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    qr_node = StateMachine()
+    node = StateMachine()
 
-    rclpy.spin(qr_node)
+    rclpy.spin(node)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    qr_node.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 
