607e6a1a0251bedee7a6e2c32fd7c3e760ea9e9b
RaspberryPI <sdp8@raspberrypi.local>
2023-02-13 12:24:56 +0000

    launch file for all the packages

---
+++ launch/box.launch.py
@@ -0,0 +1,34 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from ament_index_python import get_package_share_directory
+
+def generate_launch_description():
+    qr_pipeline = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('qr_scanner_pipeline'), 'qr_scanner.launch.py'
+        )]),
+    )
+    
+    door_lock_node = Node(
+        name='lock_service',
+        package='door_lock',
+        executable='door_lock',
+        output='both',
+    )
+
+    state_machine = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('state_machine'), 'state_machine.launch.py'
+        )]),
+    )
+
+    return LaunchDescription([
+        qr_pipeline, 
+        door_lock_node, 
+        state_machine
+    ])
