7105d00c3e7dd0dc08e99940ff78defe4eb02344
Josue Fleitas <josue.fle.sanc@gmail.com>
2023-03-03 13:52:41 +0000

    Refactors Backend to use REST API

--- src/qr_verify/test.py
+++ src/qr_verify/test.py
@@ -6,21 +6,21 @@ import unittest
 
 class TestQrVerify(unittest.TestCase):
 
-    def test_verify_emulated(self):
-        verifier = QRVerify(hostname="localhost", host_port=9000)
-        self.assertTrue(verifier.verify("12345678910+432f45b44c432414d2f97df0e5743818"))
+    def test_verify(self):
+        verifier = QRVerify(hostname="joflesan-ubuntu.local", host_port=5000)
+        self.assertTrue(verifier.verify("63f52275b2422530719ec323+averysecurehash"))
 
-    def test_verify_cloud(self):
-        verifier = QRVerify()
-        self.assertTrue(verifier.verify("12345678910+432f45b44c432414d2f97df0e5743818"))
-
-    def test_verify_not_found(self):
-        verifier = QRVerify()
+    def test_verify_connection_err(self):
+        verifier = QRVerify(hostname="fake", host_port=3000)
         self.assertRaises(ConnectionError, verifier.verify, "imadethisup+fakehash")
 
+    def test_verify_delivery_not_found(self):
+        verifier = QRVerify(hostname="joflesan-ubuntu.local", host_port=5000)
+        self.assertRaises(LookupError, verifier.verify, "fakeithink+verysecureindeed")
+
     def test_verify_hash_invalid(self):
-        verifier = QRVerify()
-        self.assertFalse(verifier.verify("12345678910+fakehash"))
+        verifier = QRVerify(hostname="joflesan-ubuntu.local", host_port=5000)
+        self.assertFalse(verifier.verify("63f52275b2422530719ec323+fakehash"))
 
 if __name__ == "__main__":
     unittest.main()
