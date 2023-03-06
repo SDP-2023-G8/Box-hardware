a432e9fa3e153487695177d48c5034f47457c52e
Cong-Xiong <andy.xiongcong@gmail.com>
2023-03-06 14:58:18 +0000

    add speaker

---
+++ src/speaker/speaker/speaker.py
@@ -0,0 +1,35 @@
+from std_srvs.srv  import SetBool
+import os
+import rclpy
+from rclpy.node import Node
+
+
+class SpeakerService(Node):
+
+    def __init__(self):
+        super().__init__('speaker')
+        self.srv = self.create_service(SetBool, 'speaker', self.callback)
+
+    def callback(self, request, response):
+        if (request.data == True):
+            self.get_logger().info('[ Sound ] door open')
+            os.system('aplay ./examples/speaker/sounds/door_open.wav')
+        else:
+            self.get_logger().info('[ Sound ] alarm')
+            os.system('aplay ./examples/speaker/sounds/alarm.wav')
+        response.success = True 
+        return response
+
+
+def main():
+    rclpy.init()
+
+    speaker_service = SpeakerService()
+
+    rclpy.spin(speaker_service)
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
