a36a03f25db955faaf5dc699b8682e193bc1ddbb
Wojciech Boncela <w.boncela@gmail.com>
2023-03-25 14:55:21 +0000

    Acc alarm (#45)
    * init
    
    * alarm playing
    
    * multithreaded executor
    
    * tested
    
    * tested
    
    * comments
    
    * alarm triggered without subscribers
    
    * fix
    
    * fix

--- src/accelerometer/accelerometer/accelerometer.py
+++ src/accelerometer/accelerometer/accelerometer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import math
+import os
+
 import rclpy
 from rclpy.node import Node
 
@@ -12,22 +15,38 @@ import busio
 import adafruit_lis3dh
 
 class Pub_accelerometer(Node):
-
     ACC_PUB_TOPIC = '~/acc'
+
+    # Motion detection
+    THRESHOLD = 0.5
+    # At least N_ABOVE_THRESHOLD samples above threshold among last WINDOWS_SIZE samples
+    WIND0W_SIZE = 4
+    N_ABOVE_THRESHOLD = 2
+    G = 9.81
+    
+
+    def is_moving(self):
+        above_th_count = 0
+        for acc in self.past_samples:
+            if abs(self.G - acc) > self.THRESHOLD:
+                above_th_count += 1
+            if above_th_count >= self.N_ABOVE_THRESHOLD:
+                return True
+        return False
 
     def __init__(self):
         super().__init__('accelerometer')
+        self.past_samples = [self.G for _ in range(self.WIND0W_SIZE)]
+        self.alarm_playing = False
         self.i2c = busio.I2C(board.SCL, board.SDA) 
         self.int1 = digitalio.DigitalInOut(board.D24)
         self.publisher_ = self.create_publisher(AccelStamped, self.ACC_PUB_TOPIC, 10)
-        timer_period = 0.05     # 20Hz
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.timer_period = 0.05     # 20Hz
+        self.acc_timer_ = self.create_timer(self.timer_period, self.timer_callback)
         self.get_logger().info(
-            "Accelererometer publisher topic: {0}. Publishing frequency: {1}Hz.".format(self.publisher_.topic_name, 1e9 / self.timer.timer_period_ns))
+            "Accelererometer publisher topic: {0}. Publishing frequency: {1}Hz.".format(self.publisher_.topic_name, 1e9 / self.acc_timer_.timer_period_ns))
 
     def timer_callback(self):
-        if self.publisher_.get_subscription_count() == 0:
-            return
         msg = AccelStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
 
@@ -38,22 +57,33 @@ class Pub_accelerometer(Node):
         msg.accel.linear.y = y
         msg.accel.linear.z = z
 
-        self.publisher_.publish(msg)
-        self.get_logger().debug('Publishing: "%s"' % msg)
+        acc_value = math.sqrt(x*x + y*y + z*z)
+        self.past_samples.pop(0)
+        self.past_samples.append(acc_value)
+        if not self.alarm_playing and self.is_moving():
+            self.get_logger().info('Triggering the alarm')
+            self.alarm_playing = True
+            self.destroy_timer(self.acc_timer_)
+            self.play_timer_ = self.create_timer(0, lambda: self.play_alarm_callback())
 
+        if self.publisher_.get_subscription_count() > 0:
+            self.get_logger().debug('Publishing: "%s"' % msg)
+            self.publisher_.publish(msg)
+
+    def play_alarm_callback(self):
+        self.destroy_timer(self.play_timer_)
+        os.system('aplay ./examples/speaker/sounds/alarm.wav -D default:CARD=UACDemoV10')
+        self.alarm_playing = False
+        self.acc_timer_ = self.create_timer(self.timer_period, self.timer_callback)
 
 def main():
     rclpy.init()
 
     pub_accelerometer = Pub_accelerometer()
 
-    rclpy.spin(pub_accelerometer)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    pub_accelerometer.destroy_node()
-    rclpy.shutdown()
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(pub_accelerometer);
+    executor.spin()
 
 if __name__ == '__main__':
     main()
